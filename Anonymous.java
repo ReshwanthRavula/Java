f4c90f62907f5b79cce4cf899f37abb4f91d2f9e
ReshwanthRavula <ravulareshwanth@gmail.com>
2024-12-25 07:18:23 -0500

    anonymous

---
+++ Anonymous.java
@@ -0,0 +1,17 @@
+class A {
+    public A() {
+        System.out.println("object created");
+    }
+
+    public void show() {
+        System.out.println("in A show");
+    }
+
+}
+
+public class Anonymous {
+    public static void main(String[] args) {
+        new A(); // anonymous object
+        new A().show();
+    }
+}
