48af52a37cb204d17ee178d29390c9ab3b5f910f
ReshwanthRavula <ravulareshwanth@gmail.com>
2024-12-25 19:38:37 -0500

    jk iifvr

--- Inheritance/Caluclator.java
+++ Inheritance/Caluclator.java
@@ -3,14 +3,16 @@ package Inheritance;
 
 public class Caluclator {
     public static void main(String[] args) {
-        AdvanceCalc B = new AdvanceCalc();
+        VeryAdvaceCal B = new VeryAdvaceCal();
     
         int a = B.addition(3, 5);
         int b = B.substraction(7, 3);
         int c = B.mul(3,4);
         int d = B.division(15,3); // method from different class AdvanceCalc
+        double e = B.power(2, 3);
 
-        System.out.printf("Add: %d, Sub: %d, Mul: %d, Div: %d%n", a, b, c, d);
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e);
+
         /// Reshwanth Ravula thinava  
     }
     
---
+++ Inheritance/VeryAdvaceCal.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class VeryAdvaceCal extends AdvanceCalc{
+
+    public double power(double a, double b){
+
+        return Math.pow(a, b);
+    }
+    
+}
